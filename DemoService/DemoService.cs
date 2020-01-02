ec153b40aa44618f177d3e758a7aff52f50ef355
MScholtes <markusscholtes@hotmail.com>
2020-01-02 16:33:29 +0100

    Initial commit

--- DemoService/DemoService.cs
+++ DemoService/DemoService.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly:AssemblyConfiguration("")]
 [assembly:AssemblyCompany("MS")]
 [assembly:AssemblyProduct("DemoService")]
-[assembly:AssemblyCopyright("© Markus Scholtes 2019")]
+[assembly:AssemblyCopyright("© Markus Scholtes 2020")]
 [assembly:AssemblyTrademark("")]
 [assembly:AssemblyCulture("")]
 [assembly:AssemblyVersion("1.0.0.0")]
